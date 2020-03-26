63640339c9bdba672862a21d4d8f23e8d0868de6
brad phelan <brad@moduleworks.com>
2020-03-26 14:53:23 +0100

    Add error handling for parse combinators

--- src/test/Tests.fs
+++ src/test/Tests.fs
@@ -24,8 +24,8 @@ module Parsers =
         ( fun (v:int) -> sprintf "%d" v), 
           fun (txt:string) -> 
             match run pint32 txt with 
-            | Success(result,_,_)->Some result
-            | Failure _->None
+            | Success(result,_,_)->Result.Ok result
+            | Failure _->Result.Error "unable to parse"
         )
 
     let intAsync = (
@@ -34,8 +34,8 @@ module Parsers =
             async {
                 do! Async.Sleep 1000
                 match run pint32 txt with 
-                | Success(result,_,_)->return Some result
-                | Failure _->return None
+                | Success(result,_,_)->return Result.Ok result
+                | Failure _->return Result.Error "unable to parse"
             } 
             |> Async.StartAsTask
             |> Async.AwaitTask
@@ -55,25 +55,58 @@ let ``basic lensing`` () =
 
 
 [<Fact>]
-let ``lensing through arrays works`` =
+let ``lensing through arrays works`` () =
     let company = BGS.Data.companyFaker.Generate ""
     let image = simpleImage(company)
     let employee2 = image >-> Company.employees' >-> (Lens.Array.at 2) >-> Person.firstName'
 
     image.Get.employees.[2].firstName |> should not' (equal "freddy krueger")
-    employee2.Set "freedy krueger"
+    employee2.Set "freddy krueger"
     image.Get.employees.[2].firstName |> should equal "freddy krueger"
 
 
+
 [<Fact>]
-let ``Parsing should work`` =
+let ``Tuple of lenses to a lens of tuples``() =
+    let company = BGS.Data.companyFaker.Generate ""
+
+    let ci = simpleImage(company)
+    let error = simpleImage(false)
+
+    let ie = Lens.Tuple.mk2 (ci>->Company.revenue') error
+
+    ie.Set(999, true)
+
+    ci.Get.revenue |> should equal 999
+    error.Get |> should equal true
+
+    ie.Set(123, false)
+
+    ci.Get.revenue |> should equal 123
+    error.Get |> should equal false 
+
+[<Fact>]
+let ``Parsing should work``() =
     let company = BGS.Data.companyFaker.Generate ""
     let image = simpleImage(company)
-    let revenueAsString = image >-> Company.revenue' >-> Parsers.int
+
+    let err = simpleImage("")
+
+    let errHandler = err.ToOption ""
+
+    let revenueAsString = (image >-> Company.revenue').Parse errHandler  Parsers.int
     let revenue = image >-> Company.revenue' 
     revenue.Set(25)
     revenue.Get |> should equal 25
-    revenueAsString |> should equal "25"
+    revenueAsString.Get |> should equal "25"
     revenueAsString.Set "257"
     revenue.Get |> should equal 257
-    revenueAsString |> should equal "257"
+    revenueAsString.Get |> should equal "257"
+
+    err.Get |> should equal ""
+    revenueAsString.Set "foo"
+    err.Get |> should equal "unable to parse"
+
+    revenueAsString.Set "666"
+    revenueAsString.Get |> should equal "666"
+    err.Get |> should equal ""
