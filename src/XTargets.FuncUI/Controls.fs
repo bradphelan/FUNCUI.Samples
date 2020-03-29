518c7e5ebb7f273d3b315a288fa6cc44d82b449c
brad phelan <brad@moduleworks.com>
2020-03-29 17:38:56 +0200

    refactoring

---
+++ src/XTargets.FuncUI/Controls.fs
@@ -0,0 +1,18 @@
+namespace XTargets.Elmish
+
+open Avalonia.Controls
+open Avalonia.FuncUI.DSL
+open XTargets.Elmish
+
+module TextBox =
+    let onTextInput handler =
+        [
+            TextBox.onKeyDown ( fun args ->  handler (args.Source :?> TextBox).Text  )
+            TextBox.onKeyUp ( fun args ->  handler (args.Source :?> TextBox).Text  )
+        ]
+
+    let inline bindText (lens:Image<string>) =
+        [
+            TextBox.text lens.Get
+            yield! onTextInput lens.Set
+        ]
